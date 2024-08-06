ec08f9fc2d56a0917bd935c30434770c7a0c6fb2
lOreinHetic <laurene.hetic@gmail.com>
2024-08-06 16:18:02 +0200

    Ajout fonctionnalité pour enregistrer en local la création des pokémons

---
+++ js/create_team.js
@@ -0,0 +1,10 @@
+document.getElementById('team-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const name = document.getElementById('pokemon-name').value;
+    const hp = parseInt(document.getElementById('pokemon-hp').value, 10);
+    const attack = parseInt(document.getElementById('pokemon-attack').value, 10);
+
+    const playerPokemon = { name, hp, attack };
+    localStorage.setItem('playerPokemon', JSON.stringify(playerPokemon));
+    document.getElementById('start-battle').style.display = 'block';
+});
