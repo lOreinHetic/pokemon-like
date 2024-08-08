87b62144a9953c9194a9584400a20d75025b4ec3
lOreinHetic <laurene.hetic@gmail.com>
2024-08-08 12:00:04 +0200

    Mettre les boutons du bouton les uns en dessous des autres

--- js/menu.js
+++ js/menu.js
@@ -11,13 +11,11 @@ function goToHome() {
 }
 
 function pauseGame() {
-    // Logique pour mettre en pause le jeu
     console.log('Jeu en pause');
     alert('Jeu en pause');
 }
 
 function restartGame() {
-    // Logique pour relancer le jeu
     console.log('Recommencer le jeu');
     alert('Le jeu va recommencer');
     window.location.reload(); // Recharger la page pour recommencer le jeu
